b9cffab9d31461942da822151c77c9bfc14446c2
Vadim Hatsura <vadim.hatsura@gmail.com>
2022-09-03 13:49:38 +0200

    update windows install script

--- windows.install.ps1
+++ windows.install.ps1
@@ -6,8 +6,9 @@ param(
     [string]$OveoEmployeeEmail = "null"
 )
 
-Invoke-WebRequest -Uri "https://cdn.oveo.io/desktop-agent/Oveo+Desktop+Agent-1.1.3.exe" -OutFile "./oveo-agent.exe"
+Invoke-WebRequest -Uri "https://cdn.oveo.io/desktop-agent/Oveo+Desktop+Agent+Installer+1.2.1.msi" -OutFile "./oveo-agent-installer.msi"
 
-& './oveo-agent.exe' /VERYSILENT /LOG /OveoCustomerKey=$OveoCustomerKey /OveoEmployeeEmail=$OveoEmployeeEmail | Out-Null
+# & './oveo-agent-insaller.msi' /VERYSILENT /LOG /OveoCustomerKey=$OveoCustomerKey /OveoEmployeeEmail=$OveoEmployeeEmail | Out-Null
+& msiexec /l*v oveo.log /i 'oveo-agent-installer.msi' /passive /qn OVEO_CUSTOMER_KEY=$OveoCustomerKey OVEO_EMPLOYEE_EMAIL=$OveoEmployeeEmail
 
-Remove-Item "./oveo-agent.exe"
+Remove-Item "./oveo-agent-installer.msi"

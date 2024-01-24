ab1908652289793d397b6f5a61d8226d82ada828
Dmitry Kosar <kosar.dmitry@gmail.com>
2024-01-24 17:50:14 +0100

    v1.4.2 (#18)

--- macos.install.sh
+++ macos.install.sh
@@ -5,9 +5,9 @@ set -e
 # VIIO_CUSTOMER_KEY
 # VIIO_EMPLOYEE_EMAIL
 
-PKG_URL="https://cdn.oveo.io/desktop-agent/viio-agent-1.4.1.pkg"
+PKG_URL="https://cdn.oveo.io/desktop-agent/viio-agent-1.4.2.pkg"
 # Checksum needs to be updated when PKG_URL is updated.
-CHECKSUM="4f528e307b0019e11ca812ff42581a2cffca54845fab06c24465a3378a3fc924"
+CHECKSUM="233247d9ca9ec0661cd61b7aa5698c5b5a1101966ad46c2f6ab55e34120b2e6a"
 SUPPORT_EMAIL="support@viio.io"
 DEVELOPER_ID="Oveo ApS (895LF9A7K6)"
 CERT_SHA_FINGERPRINT="D6B409F777DC4F2D2C738EF021E40CD2286A9D8F3EA83ACFE3D2D449C53AE3A2"

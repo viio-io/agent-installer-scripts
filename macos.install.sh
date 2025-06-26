47dbb90708d8d881d05694ce601feb81a18a1b8d
Vadim Hatsura <vadim.hatsura@gmail.com>
2025-06-26 13:45:40 +0200

    chore: address linter issues

--- macos.install.sh
+++ macos.install.sh
@@ -18,20 +18,20 @@ VIIO_CONF_PATH="/etc/viio.conf"
 # Viio Agent needs to be installed as root; use sudo if not already uid 0
 ##
 if [ "$UID" = "0" ]; then
-    SUDO=''
+  SUDO=''
 else
-    SUDO='sudo -E'
+  SUDO='sudo -E'
 fi
 
 if [ -z "$VIIO_CUSTOMER_KEY" ]; then
-    printf "\033[31m
+  printf "\033[31m
 You must specify the VIIO_CUSTOMER_KEY environment variable in order to install the agent.
 \n\033[0m\n"
-    exit 1
+  exit 1
 fi
 
 function onerror() {
-    printf "\033[31m%s
+  printf "\033[31m%s
 Something went wrong while installing the Viio Desktop Agent.
 If you're having trouble installing, please send an email to %s, and we'll help you fix it!
 \n\033[0m\n" "$ERROR_MESSAGE" "$SUPPORT_EMAIL"
@@ -51,10 +51,10 @@ curl --progress-bar $PKG_URL >"$PKG_PATH"
 printf "\033[34m\n* Ensuring checksums match\n\033[0m"
 downloaded_checksum=$(shasum -a256 "$PKG_PATH" | cut -d" " -f1)
 if [ "$downloaded_checksum" = $CHECKSUM ]; then
-    printf "\033[34mChecksums match.\n\033[0m"
+  printf "\033[34mChecksums match.\n\033[0m"
 else
-    printf "\033[31m Checksums do not match. Please contact %s \033[0m\n" "$SUPPORT_EMAIL"
-    exit 1
+  printf "\033[31m Checksums do not match. Please contact %s \033[0m\n" "$SUPPORT_EMAIL"
+  exit 1
 fi
 
 ##
@@ -63,10 +63,10 @@ fi
 printf "\033[34m\n* Ensuring package Developer ID matches\n\033[0m"
 
 if pkgutil --check-signature "$PKG_PATH" | grep -q "$DEVELOPER_ID"; then
-    printf "\033[34mDeveloper ID matches.\n\033[0m"
+  printf "\033[34mDeveloper ID matches.\n\033[0m"
 else
-    printf "\033[31m Developer ID does not match. Please contact %s \033[0m\n" "$SUPPORT_EMAIL"
-    exit 1
+  printf "\033[31m Developer ID does not match. Please contact %s \033[0m\n" "$SUPPORT_EMAIL"
+  exit 1
 fi
 
 ##
@@ -74,10 +74,10 @@ fi
 ##
 printf "\033[34m\n* Ensuring package Developer Certificate Fingerprint matches\n\033[0m"
 if pkgutil --check-signature "$PKG_PATH" | tr -d '\n' | tr -d ' ' | grep -q "SHA256Fingerprint:$CERT_SHA_FINGERPRINT"; then
-    printf "\033[34mDeveloper Certificate Fingerprint matches.\n\033[0m"
+  printf "\033[34mDeveloper Certificate Fingerprint matches.\n\033[0m"
 else
-    printf "\033[31m Developer Certificate Fingerprint does not match. Please contact %s \033[0m\n" "$SUPPORT_EMAIL"
-    exit 1
+  printf "\033[31m Developer Certificate Fingerprint does not match. Please contact %s \033[0m\n" "$SUPPORT_EMAIL"
+  exit 1
 fi
 
 ##
@@ -98,8 +98,8 @@ rm -f "$PKG_PATH"
 # check if the agent is running
 ##
 if launchctl print system/io.viio.agent.metalauncher | grep -q "state = running"; then
-    printf "\033[32mYour Agent is running properly. It will continue to run in the background and submit data to Viio.\033[0m"
+  printf "\033[32mYour Agent is running properly. It will continue to run in the background and submit data to Viio.\033[0m"
 else
-    printf "\033[31m The Viio Desktop Agent is not running after installation. Please contact %s \033[0m\n" "$SUPPORT_EMAIL"
-    exit 1
+  printf "\033[31m The Viio Desktop Agent is not running after installation. Please contact %s \033[0m\n" "$SUPPORT_EMAIL"
+  exit 1
 fi
